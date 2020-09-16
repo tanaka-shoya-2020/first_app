4e32569253440a8cb7d68e2fc3026f88d48d53c7
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-09-16 14:11:01 +0900

    first-commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+
+  def index#　indexアクションを定義した
+     @posts = Post.all
+    # @post = Post.find(1)
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+    # @post = Post.new
+    # @post.save
+  end
+  
+end
+
+ 
